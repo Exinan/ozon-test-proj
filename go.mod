2b895e8c554ec4c4c2ba025b401cb97c0dee03a8
Exinan <Exinanx@gmail.com>
2023-12-15 21:20:04 +0300

    add all

---
+++ go.mod
@@ -0,0 +1,24 @@
+module ozon_test
+
+go 1.21.4
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/jackc/pgx/v4 v4.18.1
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"crypto/sha256"
+	"database/sql"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	log "github.com/sirupsen/logrus"
+)
+
+var db *sql.DB
+
+const (
+	logFileName = "log.log"
+	serverPort  = 7070
+
+	dbHost     = "localhost"
+	dbPort     = 8080
+	dbUser     = "postgres"
+	dbPassword = "pass"
+	dbName     = "postgres"
+	dbSslMode  = "prefer"
+)
+
+func main() {
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// open log file
+	file, errr := os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if errr == nil {
+		// Set the file as the output for logging
+		log.SetOutput(file)
+		log.WithFields(log.Fields{
+			"main": "opening log file",
+		}).Info("File opened successful")
+		defer file.Close()
+	} else {
+		log.Fatal("Failed to open the log file.", errr)
+	}
+
+	initDB()
+
+	server := mux.NewRouter()
+
+	// adding hendling methods
+
+	server.HandleFunc("/ping", pingHandler).Methods("GET")
+
+	server.HandleFunc("/shorten", shortenHandler).Methods("POST")
+
+	server.HandleFunc("/{shortURL}", GetOriginalURLHandler).Methods("GET")
+
+	// start the server
+
+	http.Handle("/", server)
+
+	port := fmt.Sprintf(":%d", serverPort)
+
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"main": "starting server",
+		}).Info("Error starting server:", err)
+	}
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+
+	log.WithFields(log.Fields{
+		"func": "pingHandler",
+	}).Info("Received ping request.")
+
+	w.Write([]byte("Pong! Server is up and running."))
+}
+
+func generateShortURL(originalURL string) string {
+	hasher := sha256.New()
+	hasher.Write([]byte(originalURL))
+	hashBytes := hasher.Sum(nil)
+
+	shortURLHash := hex.EncodeToString(hashBytes)[:8]
+
+	shortURL := fmt.Sprintf("%s", shortURLHash) // http://short.url/
+	return shortURL
+}
+
+func initDB() {
+	var err error
+
+	credential := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s", dbUser, dbPassword, dbHost, dbPort, dbName, dbSslMode)
+	db, err = sql.Open("pgx", credential)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"func": "initDB",
+		}).Info("Error opening sql", err)
+	}
+
+	// Ping server
+	err = db.Ping()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"func": "initDB",
+		}).Info("Error pinging server", err)
+	}
+	// id SERIAL PRIMARY KEY,
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS urls (
+			original_url TEXT NOT NULL,
+			short_url TEXT NOT NULL
+		)
+	`)
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"func": "initDB",
+		}).Info("Error creating table", err)
+	}
+}
+
+func shortenHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		log.WithFields(log.Fields{
+			"func": "shortenHandler",
+		}).Info("Method not allowed")
+		return
+	}
+
+	originalURL := r.FormValue("url")
+	if originalURL == "" {
+		http.Error(w, "Missing 'url' parameter", http.StatusBadRequest)
+		log.WithFields(log.Fields{
+			"func": "shortenHandler",
+		}).Info("Missing 'url' parameter")
+		return
+	}
+
+	shortURL := generateShortURL(originalURL)
+
+	// Save original url and shorten url in db
+	// curl -X POST -d "url=https://www.example.com" http://localhost:7070/shorten
+	_, err := db.Exec("INSERT INTO urls (original, short) VALUES ($1, $2)", originalURL, shortURL)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"func": "shortenHandler",
+		}).Info("Error inserting into database:", err)
+		http.Error(w, "Failed to save URL to database", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(shortURL))
+}
+
+func GetOriginalURLHandler(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	shortURL := vars["shortURL"]
+
+	var originalURL string
+	err := db.QueryRow("SELECT original_url FROM urls WHERE short_url = $1", shortURL).Scan(&originalURL)
+	if err != nil {
+		http.NotFound(w, r)
+		log.WithFields(log.Fields{
+			"func": "GetOriginalURLHandler",
+		}).Info("URL not found")
+		return
+	}
+
+	log.WithFields(log.Fields{
+		"func": "GetOriginalURLHandler",
+	}).Info("Original URL successful found")
+	// Redirect to original URL
+	http.Redirect(w, r, originalURL, http.StatusMovedPermanently)
+}
